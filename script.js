6098f6abc97a40a33166b77b23c414e3213930fb
gamingtahmid1yt <gamingtahmid08@gmail.com>
2025-08-12 03:01:18 +0600

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const privacyBtn = document.getElementById('privacy-btn');
     
     // Replace with your actual APK file name and path
-    const apkFileName = 'AIChatBot-v2025.08-by-Tahmid.apk';
+    const apkFileName = 'Nexora AI-v2025.08-Tahmid.apk';
     const apkFilePath = `${apkFileName}`;
     
     // Download button functionality
